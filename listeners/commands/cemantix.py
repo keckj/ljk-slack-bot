c7efd9c8c27756ad3236d77dd3b3f905e2cf6e75
Jean-Baptiste Keck <jean-baptiste.keck@univ-grenoble-alpes.fr>
2023-11-14 15:58:08 +0100

    cemantix with hints

--- listeners/commands/cemantix.py
+++ listeners/commands/cemantix.py
@@ -1,21 +1,52 @@
 from slack_bolt import Ack, Respond, Say
 from logging import Logger
 
-from utils.cemantix import CemantixSession
+from utils.cemantix_url import CemantixSession
 
 def cemantix_callback(command, ack: Ack, respond: Respond, say: Say, logger: Logger):
+    
     if not hasattr(cemantix_callback, "session"):
         cemantix_callback.session = None
     session = cemantix_callback.session
+
+    cemantix_url = f'<{CemantixSession.game_url}|{CemantixSession.game_name}>'
+    cmd = command['text'].strip()
+
     try:
         ack()
-        if session is None:
-            respond(f"Starting a cemantix session, please wait...")
-            session = CemantixSession()
-            session.start()
-            cemantix_callback.session = session
-            say(channel='games', text=f"<@{command['user_name']}> started cemantix with token {session.token}.")
+        if cmd:
+            try:
+                permile = int(cmd)
+                if permile<0 or permile>1000:
+                    raise ValueError
+                if session is None:
+                    respond(f"Cannot get hints, {CemantixSession.game_name} session is not running.")
+                elif session.guesses:
+                    guesses = list(filter(lambda guess: guess[-1] <= permile, session.guesses))
+                    if guesses:
+                        response = ""
+                        for guess in guesses[:min(len(guesses), 10)]:
+                            response += "\n" + " ".join(map(str, guess))
+                        respond(response)
+                    else:
+                        respond(f"Cannot get hints, no solutions under {permile}‰ have been found yet.")
+                else:
+                    respond("Cannot get hints, no solutions have been found yet.")
+            except ValueError:
+                respond("Invalid format, expected a number between 0 and 1000.")
         else:
-            respond(f"A cémantix session is already running, join with token {session.token}.")
+            if (session is None) or session.time_is_over:
+                respond(f"Starting a persistent {cemantix_url} session, please wait...")
+                session = CemantixSession()
+                session.start()
+                cemantix_callback.session = session
+                say(channel='games', text=f"<@{command['user_name']}> started {cemantix_url} with token {session.token}.")
+            elif session.solution_found:
+                respond(f"Today's solution was already found, use /cemantix_url [0-1000] to get hints about the solution.")
+            elif session.has_guesses:
+                word, temp, emoji, permile = session.best_guess
+                respond(f"A {cemantix_url} session is already running, join with token {session.token}, current best guess is {temp}°C {emoji} ({permile}‰).")
+            else:
+                respond(f"A {cemantix_url} session is already running, join with token {session.token}, no solutions have been found yet.")
     except Exception as e:
         logger.error(e)
--- utils/cemantix.py
+++ utils/cemantix.py
@@ -1,6 +1,6 @@
 import time
 from datetime import datetime, timedelta
-from threading import Thread
+from threading import Thread, Lock
 
 import pytz
 
@@ -8,27 +8,73 @@ from bs4 import BeautifulSoup
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import WebDriverException
 
 class CemantixSession(Thread):
+    timezone = pytz.timezone('Europe/Paris')
+    game_name = 'cémantix'
+    game_url = "https://cemantix.certitudes.org/"
     delay = 0.1
 
     def __init__(self):
         super().__init__()
+
         self.driver = None
         self.token = None
         self.guesses = None
 
+        self.lock = Lock()
+
+        self.game_end_time = self.determine_game_end_time()
+
         self.create_session()
 
     def __del__(self):
-        if self.driver is not None:
-            self.driver.close()
-            self.driver = None
+        self.close()
+
+    @property
+    def has_guesses(self):
+        return self.guesses and self.guesses[0] 
+
+    @property
+    def best_guess(self):
+        return self.guesses[0] if self.has_guesses else None
+
+    @property
+    def solution_found(self):
+        return self.has_guesses and self.best_guess[-1] == 1000
+
+    @property
+    def time_is_over(self):
+        return self.remaining_time().total_seconds() <= 0
+
+    @property
+    def game_is_over(self):
+        return self.solution_found or self.time_is_over
+    
+    @classmethod
+    def now(cls):
+        return datetime.now(cls.timezone)
+    
+    @classmethod
+    def determine_game_end_time(cls):
+        tomorrow = cls.now().date() + timedelta(days=1)
+        midnight = cls.timezone.localize(datetime(tomorrow.year, tomorrow.month, tomorrow.day, 0, 0, 0))
+        return midnight 
+
+    def remaining_time(self):
+        return self.game_end_time - self.now()
+    
+    def remaining_time_str(self):
+        remaining_time = self.remaining_time()
+        hours, remainder = divmod(remaining_time.seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        return f"{hours}h {minutes}min {seconds}s"
 
     def create_session(self):
         self.driver = driver = webdriver.Firefox()
 
-        driver.get("https://cemantix.certitudes.org/")
+        driver.get(self.game_url)
 
         time.sleep(self.delay)
         driver.find_element(By.ID, 'dialog-close').click()
@@ -44,47 +90,58 @@ class CemantixSession(Thread):
 
         time.sleep(self.delay)
         driver.find_element(By.ID, 'dialog-close').click()
+        
+        self.running = True
+        
+        return self
 
+    def close(self):
+        with self.lock:
+            self.running = False
+            if self.driver is not None:
+                self.driver.close()
+                self.driver = None
         return self
 
     def run(self):
-        self._pretty_print_time_to_midnight()
-
         prev_state = self.driver.find_element_by_id('cemantix-guessable').get_attribute('outerHTML')
 
-        while True:
-            time.sleep(10)
+        while not self.game_is_over:
+            # make the thread sleep 10s
+            for i in range(100):
+                time.sleep(0.1)
+                if not self.running:
+                    return
+
+            self.lock.acquire()
+            if self.driver is None:
+                return
+            
+            # click on reveal solution if solution was found
+            try:
+                if btn := self.driver.find_element(By.ID, 'cemantix-reveal'):
+                    btn.click()
+                    time.sleep(self.delay)
+                if btn := self.driver.find_element(By.ID, 'cemantix-guess-btn'):
+                    btn.click()
+                    time.sleep(self.delay)
+            except WebDriverException:
+                pass
+            
+            # extract current guesses
             cur_state = self.driver.find_element_by_id('cemantix-guessable').get_attribute('outerHTML')
             if cur_state != prev_state:
                 soup = BeautifulSoup(cur_state, 'html.parser')
                 guesses = self._extract_guesses(soup)
                 self.guesses = guesses
-                print('-----------')
-                for guess in guesses:
-                    print(guess)
-                if guesses[0][-1] == 1000:
-                    print("Word found, killing session...")
-                    break
                 prev_state = cur_state
 
-        self.driver.close()
-        self.driver = None
-
-    def _pretty_print_time_to_midnight(self):
-        paris_tz = pytz.timezone('Europe/Paris')
-        now = datetime.now(paris_tz)
-
-        tomorrow = now.date() + timedelta(days=1)
-        midnight = paris_tz.localize(datetime(tomorrow.year, tomorrow.month, tomorrow.day, 0, 0, 0))
-
-        remaining_time = midnight - now
-
-        hours, remainder = divmod(remaining_time.seconds, 3600)
-        minutes, seconds = divmod(remainder, 60)
-
-        print(f"Remaining time: {hours}h {minutes}min {seconds}s")
-
-    def _extract_guess(self, soup):
+            self.lock.release()
+        
+        return self.close()
+    
+    @classmethod
+    def _extract_guess(cls, soup):
         word = soup.find('td', {'class': 'word'})
         if not word:
             return None
@@ -94,26 +151,27 @@ class CemantixSession(Thread):
         numbers = soup.find_all('td', {'class': 'number'})
         try:
             temperature = float(numbers[-2].get_text(strip=True))
-            pourcent = int(numbers[-1].get_text(strip=True))
+            percent = int(numbers[-1].get_text(strip=True))
         except ValueError:
             return None
 
         emoji = soup.find('td', {'class': 'emoji'}).get_text(strip=True)
 
-        return (word, temperature, emoji, pourcent)
-
-    def _extract_guesses(self, soup):
+        return (word, temperature, emoji, percent)
+    
+    @classmethod
+    def _extract_guesses(cls, soup):
         table = soup.find('table', {'id': 'cemantix-guessable'})
 
         top_guess_soup = table.thead.find('tr', {'class': 'guesses'})
 
-        guesses = [self._extract_guess(top_guess_soup)]
+        guesses = [cls._extract_guess(top_guess_soup)]
         for row in table.tbody.find_all('tr'):
-            guesses.append(self._extract_guess(row))
+            guesses.append(cls._extract_guess(row))
 
         guesses = sorted(set(filter(bool, guesses)), key=lambda x: x[-1], reverse=True)
         return guesses
 
 if __name__ == '__main__':
     session = CemantixSession()
-    session.run()
+    session.start()

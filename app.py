84aca5d99202f68afd83b55a6880c18e578df977
Jean-Baptiste Keck <jbkeck@hotmail.com>
2024-03-31 20:12:01 +0200

    slack bot service

--- app.py
+++ app.py
@@ -6,13 +6,10 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 from listeners import register_listeners
 
-# Initialization
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 logging.basicConfig(level=logging.DEBUG)
 
-# Register Listeners
 register_listeners(app)
 
-# Start Bolt app
 if __name__ == "__main__":
     SocketModeHandler(app, os.environ.get("SLACK_APP_TOKEN")).start()
---
+++ run_ljk_slack_bot.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -feu -o pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+export SLACK_APP_TOKEN='...'
+export SLACK_BOT_TOKEN='...'
+
+PYTHON_BIN="/usr/bin/python3"
+PYTHON_SCRIPT="${SCRIPT_DIR}/app.py"
+
+${PYTHON_BIN} ${PYTHON_SCRIPT}
